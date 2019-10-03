26de61e2b8d6d8c7abee5751519e1164617a8526
Simone <s.maya.cunningham@gmail.com>
2019-10-03 08:29:40 -0700

    adding firebase link to html

---
+++ assets/javascript/train.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDLlx8G9Vd6-MIU5eauTWqwS0-wGzD33aU",
+    authDomain: "train-station-8a015.firebaseapp.com",
+    databaseURL: "https://train-station-8a015.firebaseio.com",
+    projectId: "train-station-8a015",
+    storageBucket: "",
+    messagingSenderId: "84556650498",
+    appId: "1:84556650498:web:e0e46e124ec8360baf2f1b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
